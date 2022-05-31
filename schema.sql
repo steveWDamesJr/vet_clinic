2bfe2ee2bb1a7813e636d36cac5a5573a13f13ec
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-05-30 20:25:12 -0500

    Re-initialize files

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL
-);
